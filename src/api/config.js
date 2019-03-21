ffe9303e42fb2faaf099eb05166b40cfdc130eec
Andres Leonardo Arevalo Parra <Aarevalop@intergrupo.com>
2019-03-21 00:47:42 -0500

    Deploy in Github Pages!

---
+++ src/api/config.js
@@ -0,0 +1,6 @@
+export default {
+    appName: 'platzimusic',
+    apiKey: 'e3710b1dda21e2fad71401bd4a60e1be',
+    secret: 'f60ef2b889f2eb72b8d543fbc85ec273',
+    registeredTo: 'andresrah'
+}
---
+++ src/components/Artist.vue
@@ -0,0 +1,19 @@
+<template lang="pug">
+  li.artist
+    h2: a(v-bind:href="artistaa.url" target="_blank") {{ artistaa.name }}
+    img(:src="artistaa.image[2]['#text']")
+</template>
+
+<script>
+export default {
+  name: "artist",
+  props: ["artistaa"]
+};
+</script>
+
+<style lang="stylus" scoped>
+li.artist {
+  display: block;
+  margin: 10px 0;
+}
+</style>
